fa94ee80b9624a0b1a37692349ec66be8e37fdbc
icemanyk <404500553@qq.com>
2024-09-09 23:33:28 -0400

    staybooking-frontend

---
+++ src/components/MyStays.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { getStaysByHost } from "../utils";
+import { List, message } from "antd";
+
+class MyStays extends React.Component {
+  state = {
+    loading: false,
+    data: [],
+  };
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData = async () => {
+    this.setState({
+      loading: true,
+    });
+
+    try {
+      const resp = await getStaysByHost();
+      this.setState({
+        data: resp,
+      });
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <List
+        loading={this.state.loading}
+        grid={{
+          gutter: 16,
+          xs: 1,
+          sm: 3,
+          md: 3,
+          lg: 3,
+          xl: 4,
+          xxl: 4,
+        }}
+        dataSource={this.state.data}
+        renderItem={(item) => {
+          return <div>{item.id}</div>;
+        }}
+      />
+    );
+  }
+}
+export default MyStays;
